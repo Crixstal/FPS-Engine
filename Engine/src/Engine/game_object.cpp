65dd0a6cc79e08c281a90d281c4f88e638f58d84
Manon Méhalin <m.mehalin@student.isartdigital.com>
2021-06-04 12:08:40 +0200

    [UI] - Pause screen done

--- Engine/src/Engine/game_object.cpp
+++ Engine/src/Engine/game_object.cpp
@@ -248,7 +248,9 @@ namespace Engine
 		else if (comp == "AMMOCOUNTER")
 			Gameplay::AmmoCounter::parseComponent(*this, goStream);
 		else if (comp == "MAINMENU")
-			Gameplay::MainMenu::parseComponent(*this, goStream);
+			Gameplay::MainMenu::parseComponent(*this, goStream);		
+		else if (comp == "PAUSESCREEN")
+			Gameplay::PauseScreen::parseComponent(*this, goStream);
 		else if (comp == "GAMEMASTER")
 			Gameplay::GameMaster::parseComponent(*this, goStream);
 		else if (comp == "CAMERAMOVEMENT")
